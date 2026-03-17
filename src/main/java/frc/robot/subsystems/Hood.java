e59217f1ff5ce1b51c45deb61fd76ddfbc5d0a11
prog1 <prog1@Team74>
2026-03-17 19:05:24 -0400

    hood limits (potentiometer)

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -17,36 +17,51 @@ import com.revrobotics.spark.config.LimitSwitchConfigAccessor;
 
 public class Hood extends SubsystemBase {
     SparkMax hood = new SparkMax(Constants.ShooterConstants.HoodMotorID,MotorType.kBrushed);
+    
+    //AnalogInput stringPotInput = new AnalogInput(0);
+    AnalogPotentiometer stringPot = new AnalogPotentiometer(0, 45, 0);
+    
+    
     double hoodSpeed = Constants.ShooterConstants.hoodSpeed;
-   
-    AnalogPotentiometer stringPot = new AnalogPotentiometer(0);
     double stringPotValue = stringPot.get();
+
+    @Override
+    public void periodic() {
+      
+    }
 
     public Command TestStringPotentiometer(){
           return run(()->{
+            stringPotValue = stringPot.get();
             System.out.println(stringPotValue);
         });
     }
 
-    public Command MoveHoodIn(){
+    public Command MoveHoodUp(){
         return run(()->{
+
+            if (stringPotValue >= 10.29) {
+                hoodSpeed = 0;
+            } 
+
             hood.set(hoodSpeed * -1);
+            
         });
     }
 
-    public Command MoveHoodOut(){
+    public Command MoveHoodDown(){
         return run(()->{
+            if (stringPotValue <= 5.19) {
+               hoodSpeed = 0;
+            }
             hood.set(hoodSpeed);
-            // if (magSwitch.get() == false){
-            //     hood.set(0);
-            //     System.out.println("SWITCHED");
-            // }
         });
     }
 
     public Command StopHood(){
         return run(()->{
             hood.set(0);
+            hoodSpeed = Constants.ShooterConstants.hoodSpeed;
         });
     }
 
